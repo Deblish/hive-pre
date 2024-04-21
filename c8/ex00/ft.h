e81df50c8a594c6a9aeec4baa2ab35ddc741220c
Aaron Padilla <aapadill@student.hive.fi>
2024-04-21 14:02:17 +0300

    directory details

--- c8/ex00/ft.h
+++ c8/ex00/ft.h
@@ -1,13 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/15 08:28:26 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/15 08:31:02 by aapadill         ###   ########.fr       */
+/*   Created: 2024/04/17 14:03:30 by aapadill          #+#    #+#             */
+/*   Updated: 2024/04/17 14:05:21 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
